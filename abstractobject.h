b5769e67abe0e41153d2395d21adbc17e58425f0
Qinsong Zheng <j24zheng@uwaterloo.ca>
2023-07-24 15:56:18 -0400

    removed virtual constructor in abstractobject.h

--- abstractobject.h
+++ abstractobject.h
@@ -3,7 +3,6 @@ class AbstractObject
     int x, y;
     char token;
  public:
-    virtual AbstractObject(int x, int y, char token) = 0;
     virtual ~AbstractObject() = 0;
     int getX();
     int getY();
