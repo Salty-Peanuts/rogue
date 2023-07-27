f6f60565323df040e3045b64fc2b3ccc1f4f888e
adam <109483705+adam-han@users.noreply.github.com>
2023-07-26 22:20:23 -0400

    added isHostile and wasMoved

--- npcs/NPC.h
+++ npcs/NPC.h
@@ -1,9 +1,20 @@
 #ifndef NPC_H
 #define NPC_H
 
+#include "abstractcharacter.h"
 
+class NPC : public AbstractCharacter 
+{
+    bool hostile;
+    bool hasMoved;
 
+    public:
+        NPC();
+        ~NPC();
+        void death();
+        bool isHostile();
+        bool wasMoved();
+};
 
 #endif
-
 
