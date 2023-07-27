a228b512e039e0d303d12cbb8593af81ca413e9c
adam <109483705+adam-han@users.noreply.github.com>
2023-07-27 18:59:23 -0400

    edited direction int values to [1-8]

--- abstractcharacter.cc
+++ abstractcharacter.cc
@@ -11,35 +11,35 @@ void AbstractCharacter::move(int dir) {
     int x = getX();
     int y = getY();
 
-    if (dir == 0) {
-        setX(x - 1);
-        setY(y + 1);
-    }
     if (dir == 1) {
-        setX(x);
+        setX(x - 1);
         setY(y + 1);
     }
     if (dir == 2) {
-        setX(x + 1);
+        setX(x);
         setY(y + 1);
     }
     if (dir == 3) {
-        setX(x - 1);
-        setY(y);
+        setX(x + 1);
+        setY(y + 1);
     }
     if (dir == 4) {
-        setX(x + 1);
+        setX(x - 1);
         setY(y);
     }
     if (dir == 5) {
+        setX(x + 1);
+        setY(y);
+    }
+    if (dir == 6) {
         setX(x - 1);
         setY(y - 1);
     }
-    if (dir == 6) {
+    if (dir == 7) {
         setX(x);
         setY(y - 1);
     }
-    if (dir == 6) {
+    if (dir == 8) {
         setX(x + 1);
         setY(y - 1);
     }
