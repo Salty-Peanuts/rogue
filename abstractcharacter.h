b4a7dc1fdf90b4e5a3daedd5b0df657888c8feea
adam <109483705+adam-han@users.noreply.github.com>
2023-07-29 19:18:48 -0400

    re-organized

--- abstractcharacter.h
+++ abstractcharacter.h
@@ -14,17 +14,18 @@ class AbstractCharacter : public AbstractObject
 
     public:
         AbstractCharacter(int x, int y, char token, int hp, int atk, int def);
-        virtual int attack(AbstractCharacter *character);
-        virtual void recieveAttack(AbstractCharacter *character);
-        virtual void death();
+        
         bool isTraversible(AbstractObject *traveler);
-        virtual string identify() = 0;
-        virtual string getRace();
         void move(int dir);
-
         int &getHP();
         int &getAtk();
         int &getDef();
+
+        virtual int attack(AbstractCharacter *character);
+        virtual void recieveAttack(AbstractCharacter *character);
+        virtual void death();
+        virtual string identify() = 0;
+        virtual string getRace();
 };
 
 #endif
