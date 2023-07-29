0d3aba3a5a117da9a115a6e7c1fc1efeb4d63655
adam <109483705+adam-han@users.noreply.github.com>
2023-07-29 16:00:43 -0400

    Elf attack interaction with Drow

--- npcs/elf.cc
+++ npcs/elf.cc
@@ -7,7 +7,22 @@ Elf::Elf(int x, int y):
 
 
 void Elf::attack(AbstractCharacter* receiver) {
+    // Elfs have special interaction where they attack twice 
+        // (Except against Drow)
 
+    // attacks Drow once
+    if (receiver->getRace() == "drow") {
+        int damage = ceil((100 / (100 + receiver->getDef())) * getAtk());
+        receiver->getHP() -= damage;
+        return;
+    }
+
+    // attacks twice otherwise
+    for (int i = 0; i < 2; i++) {
+        int damage = ceil((100 / (100 + receiver->getDef())) * getAtk());
+        receiver->getHP() -= damage;
+    }
+    
 }
 
 
