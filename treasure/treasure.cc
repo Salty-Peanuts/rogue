4a19dc55e92349f82f25025ec9f09ba8721b21ae
adam <109483705+adam-han@users.noreply.github.com>
2023-07-29 19:42:56 -0400

    delete treasure folder

--- treasure/treasure.cc
+++
@@ -1,6 +0,0 @@
-#include "treasure.h"
-
-Treasure::Treasure(int value): value{value} {}
-Treasure::~Treasure() {}
-int Treasure::getValue() {return value;}
-void Treasure::setValue(int value) {value = value;}
