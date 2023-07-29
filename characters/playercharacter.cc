d2791a27c26fa6d1dcb48e9585996803fda02e05
adam <109483705+adam-han@users.noreply.github.com>
2023-07-29 14:27:17 -0400

    usePotion implementation

--- characters/playercharacter.cc
+++ characters/playercharacter.cc
@@ -1,4 +1,10 @@
 #include "playercharacter.h"
+#include "BoostAtk.h"
+#include "BoostDef.h"
+#include "PoisonHealth.h"
+#include "RestoreHealth.h"
+#include "WoundAtk.h"
+#include "WoundDef.h"
 
 using namespace std;
 
@@ -12,11 +18,33 @@ PlayerCharacter::~PlayerCharacter() {
 
 
 void PlayerCharacter::usePotion(string pot) {
-    // implement usePotion
+    PotionEffect *new_pot;
+    if (pot == "RH") {
+        new_pot = new RestoreHealth();
+    }
+    if (pot == "BA") {
+        new_pot = new BoostAtk();
+    }
+    if (pot == "BD") {
+        new_pot = new BoostDef();
+    }
+    if (pot == "PH") {
+        new_pot = new PoisonHealth();
+    }
+    if (pot == "WA") {
+        new_pot = new WoundAtk();
+    }
+    if (pot == "WD") {
+        new_pot = new WoundDef();
+    }
+
+    potion_effects.push_back(new_pot);
+    new_pot->usePotion(getHP(), getAtk(), getDef(), getMaxHP());
 }
 
 void PlayerCharacter::removePotions() {
     for (auto potion_ptr : potion_effects) {
+        potion_ptr->removePotion(getHP(), getAtk(), getDef());
         delete potion_ptr;
     }
 }
