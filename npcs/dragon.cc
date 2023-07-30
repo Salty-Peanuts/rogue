50f3109b4119fe489514523c43a509e18c5d8b01
adam <109483705+adam-han@users.noreply.github.com>
2023-07-29 22:58:36 -0400

    added dragon_hoard field to Dragon class

--- npcs/dragon.cc
+++ npcs/dragon.cc
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-Dragon::Dragon(int x, int y):
-    NPC{x, y, 'D', 150, 20, 20} {}
+Dragon::Dragon(int x, int y, DragonHoard *dragon_hoard):
+    NPC{x, y, 'D', 150, 20, 20}, dragon_hoard{dragon_hoard} {}
 
 
 int Dragon::attack(AbstractCharacter* receiver) {
